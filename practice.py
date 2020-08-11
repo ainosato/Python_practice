c5b132875e2f4b6379b2a0ae8101bdf919c0e950
ainosato <56681766+ainosato@users.noreply.github.com>
2020-08-12 01:00:51 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,99 @@
+# print(2**3) # 제곱
+# print(5%3)
+# print(abs(-5)) # 5
+# print(pow(4, 2)) # 16
+# print(max(5, 12)) # 12
+# print(round(3.14)) # 3
+
+# from math import *
+# print(floor(4.99)) # 4
+# print(ceil(3.14)) # 4
+# print(sqrt(16)) # 4 (제곱근)
+
+from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10)
+# print(int(random() * 10))
+# print(int(random() * 10))
+# print(int(random() * 10))
+# print(int(random() * 10) + 1)
+# print(int(random() * 10) + 1)
+# print(int(random() * 10) + 1)
+
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+# print(int(random() * 45) + 1)
+
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+
+
+# day = randint(4, 28)
+# print("오프라인 스터디 모임 날짜는 매월 {} 일로 선정되었습니다".format(day))
+# print(randint(0, 2))
+# print(randint(0, 2))
+# print(randint(0, 2))
+
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper()) # 대문자 여부 감지
+# print(len(python))
+# print(python.replace("Python", "Java"))
+
+# index = python.index("n") # n의 위치 반환
+# print(index)
+# index = python.index("n", index + 1)
+# print(index)
+
+# print(python.find("Java"))
+
+# # print(python.index("Java"))
+# print(python.count("n"))
+# print("a" + "b")
+# print("a", "b")
+# print("나는 %d살입니다." % 20)
+# print("나는 %s을 좋아해요" % "파이썬")
+# print("Apple은 %c로 시작해요" % "A")
+
+# print("나는 {1}색과 {0}색을 좋아해요".format("파랑", "빨강"))
+# print("나는 {color}}색과 {age}색을 좋아해요".format(age = "20", color = "빨강"))
+# age = 20
+# color = "빨강"
+# # print(f"나는 {color}색과 {age}살을 좋아해요") #v 3.7 이상부터 사용 가능
+
+# print("백문이 불여일견 \n백견이 불여일타")
+# print("저는 '이재현'입니다.")
+# print("저도 \"이재현\"입니다.")
+
+# print("C:\\Users\\ainos\\OneDrive\\Desktop\\Python_workspace")
+# print("Red Apple\rpine")
+# print("Redd\bApple")
+# print("Red\tApple")
+
+
+add = "http://naver.com"
+add1 = add[7:12]
+print(add1)
+add2 = len(add1)
+add3 = add1.count('e')
+print('{}{}{}{}'.format(add1,add2,add3,"!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
